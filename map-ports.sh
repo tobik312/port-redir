e0225be155706c4b8942d738e2318bc70d943aa1
Tobiasz Wilhelm <tobiasz.wilhelm@gmail.com>
2023-01-03 13:51:03 +0100

    Fix wrong port type mapping

--- map-ports.sh
+++ map-ports.sh
@@ -6,10 +6,10 @@ IFS='; ';
 echo "Set port's config"
 #Load and set port's config from file
 grep -v '^#' "./ports" | while read -r src dst type ; do
-    if [ "$type"="tcp" ]; then
-        redir ${src} ${dst} && echo "Successfull map TCP - ${src} to ${dst}"
-    elif [ "$type"="udp" ]; then
-        uredir ${src} ${dst} && echo "Successfull map UDP - ${src} to ${dst}"
+    if [ "$type" = "tcp" ]; then
+        redir ${src} ${dst} & echo "Successfull map TCP - ${src} to ${dst}"
+    elif [ "$type" = "udp" ]; then
+        uredir ${src} ${dst} & echo "Successfull map UDP - ${src} to ${dst}"
     else
         echo "Invalid port type - ${type}"
     fi
